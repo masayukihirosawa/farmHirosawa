20abfafb7a66bd7587ae10df999186346c7cc815
m.hirosawa <m.hirosawa0228@gmail.com>
2021-03-20 00:14:35 +0900

    画像の最適化（の練習）

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-carousel cycle max-height="500" hide-delimiters show-arrows-on-hover>
+  <v-carousel cycle max-height="500" hide-delimiters :show-arrows="false">
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
@@ -24,12 +24,6 @@ export default {
         },
         {
           img: require("@/assets/hello-slide/slide-4.jpg"),
-        },
-        {
-          img: require("@/assets/hello-slide/slide-5.jpg"),
-        },
-        {
-          img: require("@/assets/hello-slide/slide-6.jpg"),
         },
       ],
     };
