24c85e4bc1da06606e93fd8a3b71ff0ed7368328
m.hirosawa <m.hirosawa0228@gmail.com>
2021-03-21 19:10:04 +0900

    CheckOutページを更新しました。

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h3>購入ありがとうございました。</h3>
-    <v-btn :to="{ name: 'home' }">買い物を続ける</v-btn>
+    <v-container>
+      <v-card elevation="0" class="ma-auto mt-15" max-width="400">
+        <v-card-title class="pa-8 justify-center"
+          >ご購入ありがとうございました!</v-card-title
+        >
+        <v-card-actions class="pa-8 justify-center">
+          <v-btn large :to="{ name: 'home' }">買い物を続ける</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
   </div>
 </template>
