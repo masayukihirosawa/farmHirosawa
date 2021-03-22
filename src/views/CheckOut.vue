0a41cd4f286b964637ed8d7eee985a10020907c3
m.hirosawa <m.hirosawa0228@gmail.com>
2021-03-22 23:24:23 +0900

    レイアウト微調整。 これで『ご購入ありがとうございました!』が一列で表示されるスマホの種類が増えました。

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container>
       <v-card elevation="0" class="ma-auto mt-15" max-width="400">
-        <v-card-title class="pa-8 justify-center"
+        <v-card-title class="py-8 justify-center"
           >ご購入ありがとうございました!</v-card-title
         >
         <v-card-actions class="pa-8 justify-center">
