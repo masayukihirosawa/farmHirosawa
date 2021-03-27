9debf02e8e3d96006553262dce7dbaae7aa1edce
m.hirosawa <m.hirosawa0228@gmail.com>
2021-03-27 16:40:29 +0900

    footer追加

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@
     <v-main>
       <router-view />
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import AppBar from "./components/AppBar";
 import SideNav from "./components/SideNav";
+import Footer from "./components/Footer";
 export default {
   name: "App",
   components: {
     AppBar,
     SideNav,
+    Footer
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container class="d-flex justify-center mt-16">
+    <p>@ 2021 HirosawaFarm</p>
+    <v-btn class="pa-0 mt-n2" text href="https://github.com/masayukihirosawa/farmHirosawa">
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+  </v-container>
+</template>
